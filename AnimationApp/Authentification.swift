f9bd95bead12513ea29c5b69627906f9a05b18b4
akiya <akiya0524@gmail.com>
2021-07-25 23:32:36 -0700

    added first file of authentification

---
+++ AnimationApp/Authentification.swift
@@ -0,0 +1,13 @@
+//
+//  Authentification.swift
+//  AnimationApp
+//
+//  Created by Akiya Ozawa on R 3/07/25.
+//
+
+import UIKit
+
+class Authentification {
+    let name: String
+    let id: String
+}
